726e7962a40ef01ff6086baeba3625e2ec0fc331
Diordychuk Dmitry <diordychukdmitry@MacBook-Diordychuk.local>
2020-07-29 23:25:16 +0300

    math aproache to drawing line

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kdustin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/28 21:46:40 by kdustin           #+#    #+#             */
-/*   Updated: 2020/07/29 17:37:54 by kdustin          ###   ########.fr       */
+/*   Updated: 2020/07/29 23:23:54 by kdustin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,50 +22,46 @@ typedef struct	s_data {
 	int	endian;
 }		t_data;
 
-void	draw_pixel(t_data *data, int x, int y, int color)
+typedef struct	s_point {
+	int	x;
+	int	y;
+}		t_point;
+
+void	draw_pixel(t_data *data, t_point p, int color)
 {
 	char *dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = data->addr +
+		(p.y * data->line_length + p.x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
 
-void	draw_line(t_data *data, int x1, int y1, int x2, int y2, int color)
+void	draw_line_brute(t_data *data, t_point p1, t_point p2, int color)
 {
-	float y;
 	float k;
 	float b;
-	float a;
 
-	b = (y2 - y1);
-	a = (x2 - x1);
-	if (fabs(a) >= fabs(b) && a != 0)
+	if (abs(p2.x - p1.x) >= abs(p2.y - p1.y))
 	{
-		k = b / a;
-		b = (y1 * x2 - y2 * x1) / a;
-		if (x1 > x2)
-			ft_intswap(&x1, &x2);
-		while (x1 <= x2)
-		{
-			y = k * x1 + b; 
-			draw_pixel(data, x1, y, color); 
- 			x1++;
-		}
+		k = (float)(p2.y - p1.y) / (float)(p2.x - p1.x);
+		b = (float)(p1.y * p2.x - p2.y * p1.x) / (float)(p2.x - p1.x);
+		if (p1.x > p2.x)
+			ft_intswap(&p1.x, &p2.x);
+		while (p1.x <= p2.x)
+			draw_pixel(data, (t_point){p1.x++, k * p1.x + b},
+									color);
 	}
-	else if (fabs(a) < fabs(b) && b != 0)
+	else
 	{
-		k = a / b;
-		b = (x1 * y2 - x2 * y1) / b;
-		if (y1 > y2)
-			ft_intswap(&y1, &y2);
-		while (y1 <= y2)
-		{
-			y = y1 * k + b;
-			draw_pixel(data, y, y1, color);
-			y1++;
-		}
+		k = (float)(p2.x - p1.x) / (float)(p2.y - p1.y);
+		b = (float)(p1.x * p2.y - p2.x * p1.y) / (float)(p2.y - p1.y);
+		if (p1.y > p2.y)
+			ft_intswap(&p1.y, &p2.y);
+		while (p1.y <= p2.y)
+			draw_pixel(data, (t_point){k * p1.y + b, p1.y++},
+									color);
 	}
-}	
+}
 
 int	main()
 {
@@ -77,15 +73,15 @@ int	main()
 	mlx_win = mlx_new_window(mlx, 800, 600, "MLX!");
 	img.img = mlx_new_image(mlx, 800, 600);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	draw_line(&img, 300, 200, 300, 300, 0xff0000);
-	draw_line(&img, 400, 200, 300, 300, 0x00ff00);
-	draw_line(&img, 400, 300, 300, 300, 0x0000ff);
-	draw_line(&img, 400, 400, 300, 300, 0xff0000);
-	draw_line(&img, 300, 400, 300, 300, 0x00ff00);
-	draw_line(&img, 200, 400, 300, 300, 0x0000ff);
-	draw_line(&img, 200, 300, 300, 300, 0xff0000);
-	draw_line(&img, 200, 200, 300, 300, 0x00ff00);
-	draw_pixel(&img, 300, 300, 0xc46f1a);
+	draw_line_brute(&img, (t_point){310, 200}, (t_point){300, 300}, 0xff0000);
+	draw_line_brute(&img, (t_point){410, 200}, (t_point){300, 300}, 0x00ff00);
+	draw_line_brute(&img, (t_point){410, 300}, (t_point){300, 300}, 0x0000ff);
+	draw_line_brute(&img, (t_point){410, 400}, (t_point){300, 300}, 0xff0000);
+	draw_line_brute(&img, (t_point){310, 400}, (t_point){300, 300}, 0x00ff00);
+	draw_line_brute(&img, (t_point){210, 400}, (t_point){300, 300}, 0x0000ff);
+	draw_line_brute(&img, (t_point){210, 300}, (t_point){300, 300}, 0xff0000);
+	draw_line_brute(&img, (t_point){210, 200}, (t_point){300, 300}, 0x00ff00);
+	draw_pixel(&img, (t_point){300, 300}, 0xc46f1a);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 	return (0);
