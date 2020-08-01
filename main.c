a43ca8af5efe47696947031599de46b627e5ceb6
Diordychuk Dmitry <diordychukdmitry@MacBook-Diordychuk.local>
2020-08-01 22:00:29 +0300

    add some function to coordinates transformation and sphere structer and started function to find crossing with sphere

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kdustin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/01 13:31:53 by kdustin           #+#    #+#             */
-/*   Updated: 2020/08/01 19:45:20 by kdustin          ###   ########.fr       */
+/*   Updated: 2020/08/01 21:58:40 by kdustin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,16 @@ typedef struct	s_data {
 	int	endian;
 }		t_data;
 
+typedef struct	s_point2d {
+	int	x;
+	int	y;
+}		t_point2d;
+
+typedef struct		s_sphere {
+	t_vector3d	coor;
+	double		r;
+}			t_sphere;
+
 void	draw_pixel(t_data *data, int x, int y, int color)
 {
 	char *dst;
@@ -32,30 +42,66 @@ void	draw_pixel(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-t_color3d	background_color(t_ray3d r)
+t_point2d	canvas_to_screen(t_point2d c, int canvas_width, int canvas_height)
 {
-	t_vector3d	unit_direction;
-	double		t;
+	return ((t_point2d){canvas_width / 2 + c.x, canvas_height / 2 - c.y});
+}
 
-	unit_direction = unit_vec(r.direction);
-	t = 0.5 * (unit_direction.y + 1.0);
-	return (sum_vec(mul_vec_scalar((t_color3d){1.0, 1.0, 1.0}, 1.0 - t)
-			,mul_vec_scalar((t_color3d){0.5, 0.7, 1.0}, t)));
+t_point3d	canvas_to_viewport(t_point2d c, int canvas_width, int canvas_height, int viewport_width, int viewport_height, double viewport_d)
+{
+	return ((t_point3d){c.x * (viewport_width / canvas_width), c.y * (viewport_height / canvas_height), viewport_d});
+}
+
+double		get_sphere_t(t_ray3d r, t_sphere s)
+{
+	double		k1;
+	double		k2;
+	double		k3;
+	t_vector3d	oc;
+
+	oc = minus_vec(r.origin, s.coor); 
+	k1 = dot_vec(r.direction, r.direction);
+	k2 = 2 * dot_vec(oc, r.direction);
+	k3 = dot_vec(oc, oc) - s.r * s.r;
+	//Решение k1 * t^2 + k2 * t + k3 = 0
+	return (0);
 }
 
 int	main(void)
 {
+	t_point3d	origin;
+	//Рамка
+	double		focal_length = 1.0;
+	double		viewport_height = 1.0;
+	double		viewport_width = 1.0;
+
+	origin = (t_point3d){0, 0, 0};
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
 	int	width = 800;
 	int	height = 600;
 
+	//Холст
+	double		canvas_center_x = width / 2;
+	double		canvas_center_y = height / 2;		
+
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, width, height, "MLX!");
 	img.img = mlx_new_image(mlx, width, height);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	draw_pixel(&img, 100, 100, create_trgb(0, 255, 0, 0));
+	int i = 0;
+	int j = 0;
+	while (j < height)
+	{
+		i = 0;
+		while (i < width)
+		{
+			draw_pixel(&img, i, j, create_trgb(0, 255, 0, 0));
+			i++;
+		}
+		j++;
+	}
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 	return (0);
