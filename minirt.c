d98913e9c8113cb6028cae43159a8d1c42f0e228
Diordychuk Dmitry <diordychukdmitry@MacBook-Diordychuk.local>
2020-09-17 02:38:22 +0300

    screenshot bmp creater added

--- minirt.c
+++ minirt.c
@@ -6,7 +6,7 @@
 /*   By: kdustin <kdustin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/01 13:31:53 by kdustin           #+#    #+#             */
-/*   Updated: 2020/09/14 22:31:33 by kdustin          ###   ########.fr       */
+/*   Updated: 2020/09/17 02:37:14 by kdustin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -650,6 +650,84 @@ int	parse_file(char **file_content, t_vars *vars)
 	return (0);
 }
 
+char	*init_bmp(t_vars vars)
+{
+	const int header_size = 54;//54;
+	const int img_size = vars.screen.width * vars.screen.height * 3 + vars.screen.height * 2;
+	char *bmp;
+
+	if (!(bmp = (char*)ft_calloc(header_size + img_size, sizeof(char))))
+		return (NULL);
+	*(unsigned short*)bmp = *(unsigned int*)(unsigned long)"BM";
+	*(unsigned int*)(bmp + 2) = img_size + header_size;
+	*(unsigned short*)(bmp + 6) = 0u;
+	*(unsigned short*)(bmp + 8) = 0u;
+	*(unsigned int*)(bmp + 10) = header_size;
+	*(unsigned int*)(bmp + 14) = 40;
+	*(unsigned int*)(bmp + 18) = vars.screen.width;
+	*(unsigned int*)(bmp + 22) = vars.screen.height;
+	*(unsigned short*)(bmp + 26) = 1;
+	*(unsigned short*)(bmp + 28) = 24;
+	*(unsigned int*)(bmp + 30) = 0;
+	*(unsigned int*)(bmp + 34) = img_size;
+	*(unsigned int*)(bmp + 38) = 2835;
+	*(unsigned int*)(bmp + 42) = 2835;
+	*(unsigned int*)(bmp + 46) = 0;
+	*(unsigned int*)(bmp + 50) = 0;
+	int i;
+	int x;
+	int y;
+	char *dst;
+
+	i = header_size;
+	y = vars.screen.height - 1;
+	while (y > 0)
+	{
+		x = 0;
+		while (x < vars.screen.width)
+		{
+			dst = vars.data.addr + (y * vars.data.line_length + x * (vars.data.bits_per_pixel / 8));
+			*(bmp + i) = *(dst);
+			*(bmp + i + 1) = *(dst + 1);
+			*(bmp + i + 2) = *(dst + 2);
+			x++;
+			i = i + 3;
+		}
+		y--;
+	}
+	return (bmp);
+}
+
+int	save_image(t_vars vars, char **argv)
+{
+	int		fd;
+	char	*filename;
+	int		i;
+	char	*bmp;
+
+	if (!(filename = (char*)malloc(sizeof(char) * (ft_strlen(argv[1]) + 2))))
+		return (-1);
+	i = 0;
+	while (argv[1][i + 2] != '\0')
+	{
+		filename[i] = argv[1][i];
+		i++;
+	}
+	filename[i] = 'b';
+	filename[i + 1] = 'm';
+	filename[i + 2] = 'p';
+	filename[i + 3] = '\0';
+	if ((fd = open(filename, O_WRONLY | O_CREAT, S_IREAD | S_IWRITE)) < 0)
+		return (-1);
+	if (!(bmp = init_bmp(vars)))
+		return (-1);
+	write(fd, bmp, 54 + vars.screen.height * vars.screen.width * 3 + vars.screen.height * 2);
+	//free(bmp);
+	if (close(fd) < 0)
+		return (-1);
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	t_vars	vars;
@@ -676,7 +754,10 @@ int	main(int argc, char **argv)
 		return (-1);
 	mlx_put_image_to_window(vars.mlx, vars.win, vars.data.img, 0, 0);
 	mlx_hook(vars.win, 2, 1L<<0, keys_handler, &vars);
-	mlx_loop(vars.mlx);
+	if (argc == 3 && ft_strcmp(argv[2], "--save") == 0)
+		save_image(vars, argv);
+	else
+		mlx_loop(vars.mlx);
 	free_file_content(file_content);
 	return (0);
 }
